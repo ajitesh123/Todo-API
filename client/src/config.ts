d2ab4be5dd27ab4c160938ad6b35677b55ce975f
Ajitesh Abhishek <ajiteshleo@gmail.com>
2020-04-19 18:18:43 -0500

    almost complete

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '...'
+const apiId = '4763u8ams1'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'fsdn2.auth0.com',
+  clientId: 'nygi6XG3sI6rsomJaKFHFm3DmMoI0Bzy',
   callbackUrl: 'http://localhost:3000/callback'
 }
